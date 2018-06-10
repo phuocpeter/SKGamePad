4db71839f1f832ee75cd57696f3408edd8b79e44
phuocpeter19 <phuocpeter19@gmail.com>
2018-06-10 18:55:16 -0400

    Initial Commit

---
+++ SKGamePad/SKGamePad.h
@@ -0,0 +1,19 @@
+//
+//  SKGamePad.h
+//  SKGamePad
+//
+//  Created by Phuoc Tran on 2018-06-10.
+//  Copyright © 2018 Phuoc Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKGamePad.
+FOUNDATION_EXPORT double SKGamePadVersionNumber;
+
+//! Project version string for SKGamePad.
+FOUNDATION_EXPORT const unsigned char SKGamePadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKGamePad/PublicHeader.h>
+
+
